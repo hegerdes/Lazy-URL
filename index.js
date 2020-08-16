1c70a7ca5af3378f751caf0a34ffc74c30a69ec2
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2020-08-16 21:28:13 +0200

    Added Error handling

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const yup = require('yup')
+const nanoid = require('nanoid')
+
+const schema = yup.object().shape({
+    name: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+
+});
 
 const app = express();
 app.use(helmet())
@@ -20,10 +28,27 @@ app.get('/:id', (req, res) =>{
     //TODO
 });
 
-app.post('/url', (req, res) =>{
-    //TODO
+app.post('/url', async (req, res, next) =>{
+    const {name, url } = req.body;
+    try{
+        await schema.validate({
+            name,url
+        });
+    }catch(error){
+        next(error);
+    }
 });
 
+app.use((error, req, res, next)=>{
+    if(error.status){
+        res.status(error.status);
+    }else res.status(500);
+    res.json({
+        message: error.message,
+        stack:  error.stack
+    })
+}
+)
 
 
 const port = process.env.PORT || 4242
